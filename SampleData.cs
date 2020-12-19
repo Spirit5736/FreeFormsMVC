c8c1ff923fd7eb8343b71343e3967b8e482a3401
Spirit5736 <kozhevnikovi@mail.ru>
2020-12-19 18:15:04 +0700

    First version

---
+++ SampleData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FreeFormsMVC.Models;
+
+namespace FreeFormsMVC
+{
+    public class SampleData
+    {
+        public static void Initialize(PersonContext context)
+        {
+            if
+                (!context.Persons.Any())
+            {
+                context.Persons.AddRange
+                    (
+                    new Person
+                    {
+                        Name = "Иван",
+                        Surname = "Иванов",
+                        Patronymic = "Иванович",
+                    }
+                    );
+                context.SaveChanges();
+            }
+        }
+    }
+}
